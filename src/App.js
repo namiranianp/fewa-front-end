e729109a2c62f429b4dfb8fb9172a5c803e5cafd
namiranianp <namiranianp@gmail.com>
2021-02-14 02:28:34 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from './components/general/Button.js';
 
 function App() {
   return (
@@ -9,14 +10,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button />
+        
       </header>
     </div>
   );
---
+++ src/components/general/Button.js
@@ -0,0 +1,11 @@
+import './css/Button.css';
+
+function Button() {
+  return (
+    <div className="Button">
+      <h1>Hiya</h1>
+    </div>
+  );
+}
+
+export default Button;
