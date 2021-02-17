81cac16edb35369699ecb7e546b9d3a6b7932341
Ian Murphy <ipmurphywork@gmail.com>
2021-02-17 14:06:57 -0800

    Re-organized button js and css, and set a new standard java-esque style

--- src/App.js
+++ src/App.js
@@ -1,37 +1,16 @@
+import React from 'react';
 import './App.css';
+import './components/general/css/Button.css';
+import * as Buttons from './components/general/Button.js';
 
 function App() {
 	return (
 		<div class="topBar">
-			<SearchButton/>
-			<DirectoryButton/>
-			<MenuButton/>
+			<Buttons.SearchButton/>
+			<Buttons.DirectoryButton/>
+			<Buttons.MenuButton/>
 		</div>
 	);
 }
 
 export default App;
-
-function SearchButton() {
-  return (
-    <div className="SearchButton">
-      <h1>Search</h1>
-    </div>
-  );
-}
-
-function DirectoryButton() {
-  return (
-    <div className="DirectoryButton">
-      <h1>Directory</h1>
-    </div>
-  );
-}
-
-function MenuButton() {
-  return (
-    <div className="MenuButton">
-      <h1>Menu</h1>
-    </div>
-  );
-}
--- src/components/general/Button.js
+++ src/components/general/Button.js
@@ -1,11 +1,60 @@
+/* TODO */
+/* eslint-disable no-useless-constructor */
+import React from 'react';
 import './css/Button.css';
 
-function Button() {
-  return (
-    <div className="Button">
-      <h1>Hiya</h1>
-    </div>
-  );
+export class Button extends React.Component {
+    constructor(props) {
+    super(props);
+    }
+
+  render() {
+      return (
+        <div className="Button">
+          <h1>Hiya</h1>
+        </div>
+        );
+  }
 }
 
-export default Button;
+export class SearchButton extends React.Component {
+    constructor(props) {
+    super(props);
+    }
+
+  render() {
+      return (
+        <div className="Button">
+          <h1>Search</h1>
+        </div>
+        );
+  }
+}
+
+export class DirectoryButton extends React.Component {
+    constructor(props) {
+    super(props);
+    }
+
+  render() {
+      return (
+        <div className="Button">
+          <h1>Directory</h1>
+        </div>
+        );
+  }
+}
+
+export class MenuButton extends React.Component {
+    constructor(props) {
+    super(props);
+    }
+
+  render() {
+      return (
+        <div className="Button">
+          <h1>Menu</h1>
+        </div>
+        );
+  }
+}
