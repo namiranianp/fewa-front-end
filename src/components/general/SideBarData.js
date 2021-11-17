b63a13979c630c2347fa56abe3bd9712112012d7
Rhea Arora <rheamarora@gmail.com>
2021-11-17 14:40:41 -0800

    side bar

---
+++ src/components/general/SideBarData.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import * as IoIcons from 'react-icons/io';
+
+export const SidebarData = [
+  {
+    title: 'F1',
+    path: '/suggestedfile1',
+    icon: <AiIcons.AiFillHome />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'F2',
+    path: '/suggestedfile1',
+    icon: <IoIcons.IoIosPaper />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Home',
+    path: '/home',
+    icon: <FaIcons.FaCartPlus />,
+    cName: 'nav-text'
+  },
+];
